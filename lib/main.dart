d6a4d962686b651d4a1c312d84da9b4fe77d4f24
d-shah1 <49581351+d-shah1@users.noreply.github.com>
2019-06-24 11:30:26 +0530

    All the files of the app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notekeep/screens/note_list.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      title: 'NoteKeeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+    home: NoteList(),
+    );
+  }
+}
